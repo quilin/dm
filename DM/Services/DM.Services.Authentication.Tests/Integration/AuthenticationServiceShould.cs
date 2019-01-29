6035867e280f9f33fd04226eb59bc233cc361f50
Quilin <v.quilin@gmail.com>
2019-01-29 10:00:47 +0300

    Some minor tests changes

--- DM/Services/DM.Services.Authentication.Tests/Integration/AuthenticationServiceShould.cs
+++ DM/Services/DM.Services.Authentication.Tests/Integration/AuthenticationServiceShould.cs
@@ -21,18 +21,20 @@ namespace DM.Services.Authentication.Tests.Integration
     {
         private readonly AuthenticationService authenticationService;
         private readonly DmMongoClient dmMongoClient;
+        private readonly DmDbContext dmDbContext;
 
         public AuthenticationServiceShould()
         {
             dmMongoClient = new DmMongoClient("mongodb://localhost:27017/dm3release?maxPoolSize=1000");
+            dmDbContext = new DmDbContext(new DbContextOptionsBuilder<DmDbContext>()
+                .UseNpgsql(
+                    "User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=dm3;Pooling=true;MinPoolSize=0;MaxPoolSize=100;Connection Idle Lifetime=60;")
+                .Options);
             authenticationService = new AuthenticationService(
                 new SecurityManager(new SaltFactory(), new HashProvider()),
                 new TripleDesSymmetricCryptoService(),
                 new AuthenticationRepository(
-                    new DmDbContext(new DbContextOptionsBuilder<DmDbContext>()
-                        .UseNpgsql(
-                            "User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=dm3;Pooling=true;MinPoolSize=0;MaxPoolSize=100;Connection Idle Lifetime=60;")
-                        .Options),
+                    dmDbContext,
                     dmMongoClient),
                 new SessionFactory(new GuidFactory(), new DateTimeProvider()),
                 new DateTimeProvider());
@@ -41,6 +43,14 @@ namespace DM.Services.Authentication.Tests.Integration
         [Fact]
         public async Task CreateNewSessionIfNotPresent()
         {
+            var userId = await dmDbContext.Users.AsNoTracking()
+                .Where(u => u.Login == "Quilin")
+                .Select(u => u.UserId)
+                .FirstAsync();
+            await dmMongoClient.GetCollection<UserSessions>()
+                .FindOneAndDeleteAsync(new FilterDefinitionBuilder<UserSessions>()
+                    .Eq(s => s.Id, userId));
+            
             var actual = await authenticationService.Authenticate("Quilin", "gVczyBCtus5m", true);
 
             actual.Error.Should().Be(AuthenticationError.NoError);
@@ -55,6 +65,16 @@ namespace DM.Services.Authentication.Tests.Integration
             userSessions.Sessions.First().Should().BeEquivalentTo(actual.Session, options => options
                 .Using<DateTime>(ctx => ctx.Subject.Should().BeCloseTo(ctx.Expectation, TimeSpan.FromSeconds(1)))
                 .When(info => info.SelectedMemberInfo.MemberType == typeof(DateTime)));
+        }
+
+        [Fact]
+        public async Task ReturnDecryptableToken()
+        {
+            var result = await authenticationService.Authenticate("Quilin", "gVczyBCtus5m", true);
+            result.Error.Should().Be(AuthenticationError.NoError);
+
+            var actual = await authenticationService.Authenticate(result.Token);
+            actual.Error.Should().Be(AuthenticationError.NoError);
         }
     }
 }
