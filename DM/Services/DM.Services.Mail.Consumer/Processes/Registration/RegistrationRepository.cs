999e0868103f354f913e2dc010c75b60b7b1c751
Quilin <v.quilin@gmail.com>
2019-04-27 00:23:06 +0300

    Registration fix

---
+++ DM/Services/DM.Services.Mail.Consumer/Processes/Registration/RegistrationRepository.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DM.Services.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace DM.Services.Mail.Consumer.Processes.Registration
+{
+    /// <inheritdoc />
+    public class RegistrationRepository : IRegistrationRepository
+    {
+        private readonly DmDbContext dbContext;
+
+        /// <inheritdoc />
+        public RegistrationRepository(
+            DmDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+        /// <inheritdoc />
+        public Task<RegistrationMailViewModel> Get(Guid userId)
+        {
+            return dbContext.Users
+                .Where(u => u.UserId == userId)
+                .Select(u => new RegistrationMailViewModel
+                {
+                    Email = u.Email,
+                    Login = u.Login,
+                    Token = u.Tokens
+                        .Where(t => !t.IsRemoved)
+                        .Select(t => t.TokenId)
+                        .First()
+                })
+                .FirstAsync();
+        }
+    }
+}
