f9667a229aea53579479dac9964283a87a29897e
Quilin <v.quilin@gmail.com>
2019-06-18 11:25:43 +0300

    Moves new frontend project to Modern folder adding legacy frontend project as Classic

---
+++ DM/Web/DM.Web.Classic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DM.Web.Classic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
