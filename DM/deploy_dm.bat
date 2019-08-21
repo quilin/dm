c41087696096e4b5caec9fb6e36555290699bcc9
Quilin <vladislav.kilin@monopoly.su>
2019-08-21 18:50:54 +0300

    Deployment batch file added

---
+++ DM/deploy_dm.bat
@@ -0,0 +1,9 @@
+docker build -t dm-api .
+
+rem for local run
+rem docker run -it --rm -p 5000:80 --name dm_api dm-api
+
+docker tag dm-api registry.heroku.com/dmam/web
+heroku container:push web -a dmam
+heroku container:release web -a dmam
+pause
