e44f689e84cc90561951d00a2c70871ce45a8c92
Quilin <vladislav.kilin@monopoly.su>
2019-10-17 19:28:17 +0300

    Game status transition authorization

--- DM/Services/DM.Services.Gaming/Authorization/GameIntentionResolver.cs
+++ DM/Services/DM.Services.Gaming/Authorization/GameIntentionResolver.cs
@@ -35,26 +35,44 @@ namespace DM.Services.Gaming.Authorization
         /// <inheritdoc />
         public Task<bool> IsAllowed(AuthenticatedUser user, GameIntention intention, GameExtended target)
         {
+            if (intention != GameIntention.Read && !user.IsAuthenticated)
+            {
+                return Task.FromResult(false);
+            }
+            
+            var userIsHighAuthority = user.Role.HasFlag(UserRole.Administrator) ||
+                user.Role.HasFlag(UserRole.SeniorModerator);
+            var userIsNanny = userIsHighAuthority || user.Role.HasFlag(UserRole.NannyModerator);
+            var userIsOwner = target.Master.UserId == user.UserId || target.Assistant?.UserId == user.UserId;
+            var userIsGameModerator = target.Nanny?.UserId == user.UserId;
+
             switch (intention)
             {
                 case GameIntention.Read:
-                    return Task.FromResult(
-                        user.Role.HasFlag(UserRole.Administrator) ||
-                        user.Role.HasFlag(UserRole.SeniorModerator) ||
-                        user.UserId == target.Master.UserId ||
-                        user.UserId == target.Assistant?.UserId ||
+                    return Task.FromResult(userIsHighAuthority || userIsOwner ||
                         !HiddenStates.Contains(target.Status));
                 case GameIntention.Edit:
-                    return Task.FromResult(
-                        user.Role.HasFlag(UserRole.Administrator) ||
-                        user.Role.HasFlag(UserRole.SeniorModerator) ||
-                        user.UserId == target.Master.UserId ||
-                        user.UserId == target.Assistant?.UserId);
-                case GameIntention.Delete:
-                    return Task.FromResult(
-                        user.Role.HasFlag(UserRole.Administrator) ||
-                        user.Role.HasFlag(UserRole.SeniorModerator) ||
-                        user.UserId == target.Master.UserId);
+                    return Task.FromResult(userIsHighAuthority || userIsOwner);
+                case GameIntention.Delete: // only the master itself is allowed to remove the game
+                    return Task.FromResult(userIsHighAuthority || user.UserId == target.Master.UserId);
+
+                case GameIntention.SetStatusModeration when target.Status == GameStatus.RequiresModeration:
+                    return Task.FromResult(userIsNanny);
+
+                case GameIntention.SetStatusDraft when target.Status == GameStatus.Moderation:
+                case GameIntention.SetStatusRequirement when target.Status == GameStatus.Moderation:
+                    return Task.FromResult(userIsGameModerator);
+
+                case GameIntention.SetStatusDraft when target.Status == GameStatus.Requirement:
+                case GameIntention.SetStatusRequirement when target.Status == GameStatus.Draft:
+                case GameIntention.SetStatusRequirement when target.Status == GameStatus.Active:
+                case GameIntention.SetStatusActive when target.Status == GameStatus.Frozen:
+                case GameIntention.SetStatusActive when target.Status == GameStatus.Finished:
+                case GameIntention.SetStatusActive when target.Status == GameStatus.Closed:
+                case GameIntention.SetStatusFrozen when target.Status == GameStatus.Active:
+                case GameIntention.SetStatusFinished when target.Status == GameStatus.Active:
+                case GameIntention.SetStatusClosed when target.Status == GameStatus.Active:
+                    return Task.FromResult(userIsOwner);
                 default:
                     return Task.FromResult(false);
             }
