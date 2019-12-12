e1d16f8a2de47a8b8d9feb9186db2de8cfac0a9b
Quilin <vladislav.kilin@monopoly.su>
2019-12-12 20:52:50 +0300

    Migration for minor table contract renaming

---
+++ DM/Services/DM.Services.DataAccess/Migrations/20191212174402_LinkTablesRenaming2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DM.Services.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkTablesRenaming2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PostPendingId",
+                table: "PendingPosts",
+                newName: "PendingPostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PendingPostId",
+                table: "PendingPosts",
+                newName: "PostPendingId");
+        }
+    }
+}
