567025647c7598fb9825d179d7c05569f23ec8c4
Quilin <vladislav.kilin@monopoly.su>
2019-12-16 15:40:44 +0300

    Blacklist API endpoints

--- DM/Web/DM.Web.API/Controllers/v1/Gaming/GameController.cs
+++ DM/Web/DM.Web.API/Controllers/v1/Gaming/GameController.cs
@@ -326,5 +326,61 @@ namespace DM.Web.API.Controllers.v1.Gaming
             return CreatedAtRoute(nameof(RoomController.GetRoom),
                 new {id = result.Resource.Id}, result);
         }
+
+        /// <summary>
+        /// Get game blacklisted users
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200"></response>
+        /// <response code="401">User must be authenticated</response>
+        /// <response code="403">User is not authorized to read blacklist of this game</response>
+        /// <response code="410">Game not found</response>
+        [HttpGet("{id}/blacklist/users", Name = nameof(GetBlacklist))]
+        [AuthenticationRequired]
+        [ProducesResponseType(typeof(ListEnvelope<User>), 200)]
+        [ProducesResponseType(typeof(GeneralError), 401)]
+        [ProducesResponseType(typeof(GeneralError), 403)]
+        [ProducesResponseType(typeof(GeneralError), 410)]
+        public Task<IActionResult> GetBlacklist(Guid id) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Post new blacklisted user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user"></param>
+        /// <response code="201"></response>
+        /// <response code="400">Some user properties were invalid</response>
+        /// <response code="401">User must be authenticated</response>
+        /// <response code="403">User is not authorized to blacklist users in this game</response>
+        /// <response code="409">User is already blacklisted</response>
+        /// <response code="410">Game not found</response>
+        [HttpPost("{id}/blacklist/users", Name = nameof(PostBlacklist))]
+        [AuthenticationRequired]
+        [ProducesResponseType(typeof(Envelope<User>), 201)]
+        [ProducesResponseType(typeof(BadRequestError), 400)]
+        [ProducesResponseType(typeof(GeneralError), 401)]
+        [ProducesResponseType(typeof(GeneralError), 403)]
+        [ProducesResponseType(typeof(GeneralError), 409)]
+        [ProducesResponseType(typeof(GeneralError), 410)]
+        public Task<IActionResult> PostBlacklist(Guid id, [FromBody] User user) => throw new NotImplementedException();
+
+        /// <summary>
+        /// Delete blacklisted user link
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="login"></param>
+        /// <response code="204"></response>
+        /// <response code="401">User must be authenticated</response>
+        /// <response code="403">User is not authorized to un-blacklist users in this game</response>
+        /// <response code="409">User is not in the blacklist</response>
+        /// <response code="410">Game not found</response>
+        [HttpDelete("{id}/blacklist/users/{login}", Name = nameof(DeleteBlacklist))]
+        [AuthenticationRequired]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(typeof(GeneralError), 401)]
+        [ProducesResponseType(typeof(GeneralError), 403)]
+        [ProducesResponseType(typeof(GeneralError), 409)]
+        [ProducesResponseType(typeof(GeneralError), 410)]
+        public Task<IActionResult> DeleteBlacklist(Guid id, string login) => throw new NotImplementedException();
     }
 }
