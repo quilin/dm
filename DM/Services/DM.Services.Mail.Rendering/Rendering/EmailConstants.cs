39ae513aacd02b0d50ca345d0861a8e47bdee152
Quilin <vladislav.kilin@monopoly.su>
2020-01-21 18:58:36 +0300

    Give email layout border radius

--- DM/Services/DM.Services.Mail.Rendering/Rendering/EmailConstants.cs
+++ DM/Services/DM.Services.Mail.Rendering/Rendering/EmailConstants.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DM.Services.Mail.Rendering.Rendering
 {
     /// <summary>
@@ -18,7 +21,21 @@ namespace DM.Services.Mail.Rendering.Rendering
         /// <summary>
         /// Basic button style
         /// </summary>
-        public static string ButtonStyles =>
-            "display: block; width: 300px; margin: 0 auto; padding: 12px; background-color: #36a; color: #fff; font-size: 18px; text-decoration: none; text-align: center;";
+        public static string ButtonStyles => new Dictionary<string, string>
+        {
+            ["display"] = "block",
+            ["width"] = "300px",
+            ["margin"] = "0 auto",
+            ["padding"] = "12px",
+            ["border-radius"] = "4px",
+            ["background-color"] = "#36a",
+            ["color"] = "#fff",
+            ["font-size"] = "18px",
+            ["text-decoration"] = "none",
+            ["text-align"] = "center"
+        }.ConvertStyles();
+
+        private static string ConvertStyles(this IDictionary<string, string> properties) =>
+            string.Join("; ", properties.Select(kvp => $"{kvp.Key}: {kvp.Value}"));
     }
 }
