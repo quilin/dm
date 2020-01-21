4d10e6151af58622daf63033d623eb169079fde8
Quilin <vladislav.kilin@monopoly.su>
2020-01-21 17:30:47 +0300

    Minor letter refactoring

---
+++ DM/Services/DM.Services.Mail.Rendering/Rendering/EmailConstants.cs
@@ -0,0 +1,24 @@
+namespace DM.Services.Mail.Rendering.Rendering
+{
+    /// <summary>
+    /// Some constants for email templates
+    /// </summary>
+    public static class EmailConstants
+    {
+        /// <summary>
+        /// Email layout name
+        /// </summary>
+        public static string Layout => "_EmailLayout";
+
+        /// <summary>
+        /// Basic paragraph style
+        /// </summary>
+        public static string ParagraphStyles => "margin: 16px 0;";
+
+        /// <summary>
+        /// Basic button style
+        /// </summary>
+        public static string ButtonStyles =>
+            "display: block; width: 300px; margin: 0 auto; padding: 12px; background-color: #36a; color: #fff; font-size: 18px; text-decoration: none; text-align: center;";
+    }
+}
