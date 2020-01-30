e33ab68784fb0359ca7e8c4761694326999be934
Quilin <vladislav.kilin@monopoly.su>
2020-01-30 19:33:41 +0300

    User schema migration that removes ShowEmail column

---
+++ DM/Services/DM.Services.DataAccess/Migrations/20200130163118_RemoveUserShowEmailColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DM.Services.DataAccess.Migrations
+{
+    internal partial class RemoveUserShowEmailColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowEmail",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowEmail",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
