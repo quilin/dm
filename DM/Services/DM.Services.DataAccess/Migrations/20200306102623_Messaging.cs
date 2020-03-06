d8d789276623ce0857a3701147214312641ead9d
Quilin <vladislav.kilin@monopoly.su>
2020-03-06 13:27:12 +0300

    Messaging migrations

---
+++ DM/Services/DM.Services.DataAccess/Migrations/20200306102623_Messaging.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DM.Services.DataAccess.Migrations
+{
+    internal partial class Messaging : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Users",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Skype",
+                table: "Users",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Salt",
+                table: "Users",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Users",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Icq",
+                table: "Users",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRemoved",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastMessageId",
+                table: "Conversations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Visavi",
+                table: "Conversations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conversations_LastMessageId",
+                table: "Conversations",
+                column: "LastMessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Conversations_Messages_LastMessageId",
+                table: "Conversations",
+                column: "LastMessageId",
+                principalTable: "Messages",
+                principalColumn: "MessageId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Conversations_Messages_LastMessageId",
+                table: "Conversations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Conversations_LastMessageId",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "IsRemoved",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "LastMessageId",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "Visavi",
+                table: "Conversations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Skype",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Salt",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Icq",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
