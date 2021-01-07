7c1d4b260e1f081081418553c13a29a51242383b
Quilin <vladislav.kilin@monopoly.su>
2021-01-08 02:47:32 +0300

    Поправил регистрацию модуля загрузок

--- DM/Services/DM.Services.Common/CommonModule.cs
+++ DM/Services/DM.Services.Common/CommonModule.cs
@@ -1,8 +1,13 @@
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
 using Autofac;
 using DM.Services.Authentication;
+using DM.Services.Common.Configuration;
 using DM.Services.Core;
 using DM.Services.Core.Extensions;
 using DM.Services.DataAccess;
+using Microsoft.Extensions.Options;
 using Module = Autofac.Module;
 
 namespace DM.Services.Common
@@ -15,6 +20,19 @@ namespace DM.Services.Common
         {
             builder.RegisterDefaultTypes();
             builder.RegisterMapper();
+            builder.Register<IAmazonS3>(ctx =>
+                {
+                    var cdnConfiguration = ctx.Resolve<IOptions<CdnConfiguration>>().Value;
+                    return new AmazonS3Client(
+                        new BasicAWSCredentials(cdnConfiguration.AccessKey, cdnConfiguration.SecretKey),
+                        new AmazonS3Config
+                        {
+                            ServiceURL = cdnConfiguration.Url,
+                            RegionEndpoint = RegionEndpoint.GetBySystemName(cdnConfiguration.Region)
+                        });
+                })
+                .AsSelf()
+                .SingleInstance();
 
             builder.RegisterModuleOnce<CoreModule>();
             builder.RegisterModuleOnce<AuthenticationModule>();
