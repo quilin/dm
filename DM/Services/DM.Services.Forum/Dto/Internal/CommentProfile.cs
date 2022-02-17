ec9e2d6ec459b1c8ee9b096a4642f21be0667ec4
Quilin <v.quilin@gmail.com>
2022-02-17 21:38:30 +0300

    [Issue-19] Список лайков для удаляемого комментария исключен из маппинга

--- DM/Services/DM.Services.Forum/Dto/Internal/CommentProfile.cs
+++ DM/Services/DM.Services.Forum/Dto/Internal/CommentProfile.cs
@@ -13,6 +13,7 @@ namespace DM.Services.Forum.Dto.Internal
         {
             CreateMap<Comment, CommentToDelete>()
                 .ForMember(d => d.Id, s => s.MapFrom(c => c.CommentId))
+                .ForMember(d => d.Likes, s => s.Ignore())
                 .ForMember(d => d.IsLastCommentOfTopic, s => s.MapFrom(c => c.CommentId == c.Topic.LastCommentId));
         }
     }
