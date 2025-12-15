80342a15a3123fa5051dafab69b043d5f7a10f51
quilin <v.quilin@gmail.com>
2025-12-16 00:35:58 +0100

    [DM.Front] I18n for profile page

---
+++ frontend/DM.Web.Modern.Temp/src/views/pages/profile/UserInformation.i18n.ts
@@ -0,0 +1,12 @@
+export default {
+  "ru-RU": {
+    empty: "Пользователь ничего о себе не написал...",
+    submit: "Сохранить",
+    cancel: "Отмена",
+  },
+  "en-US": {
+    empty: "The user did not write anything about themselves...",
+    submit: "Save",
+    cancel: "Cancel",
+  },
+};
---
+++ frontend/DM.Web.Modern.Temp/src/views/pages/rules/RulesPage.i18n.ts
@@ -0,0 +1,58 @@
+export default {
+  "ru-RU": {
+    title: "Правила",
+    text1:
+      "Для большинства участников проекта это не просто сайт в сети — это " +
+      "родной дом, уютное и любимое место. Чтобы не портить никому пребывание в " +
+      "этом месте, разработана система правил. Регистрируясь на сайте вы вроде как " +
+      "обязуетесь их соблюдать.",
+    text2:
+      "На самом деле, правила очень простые. Если просто мирно играть в игры, " +
+      "общаться в чате и на форуме, не пытаться ни с кем поругаться или на кого-то " +
+      "обидеться, вам никогда не придется сталкиваться с последствиями их " +
+      "нарушения!",
+    text3:
+      "Мы тут любим всякий ролевой сленг, поэтому модераторы у нас называются " +
+      '"гоблинами", а администраторы "троллями". Привыкайте, потому что дальше ' +
+      "будет про этих злобных гоблиноидов.",
+    text4:
+      "Публичные разделы сайта — темы форума, общий чат и профили " +
+      "пользователей — не место для выяснения отношений, оскорблений, мата и " +
+      "прочих прелестей интернета. За эти штуки гоблины выписывают баллы по особому " +
+      "прейскуранту. Нахватавшись сих волшебных фруктов, пользователь отправляется " +
+      "в краткое изгнание из этих публичных разделов сайта — но все еще может " +
+      "участвовать в играх.",
+    text5:
+      "Если пользователь усердствует в нарушении правил приличия, то он может " +
+      "отправиться уже в полное изгнание. Сроки изгнаний, как полного, так и, что у " +
+      'нас называется "демократичного" с каждым разом растут — вплоть до ' +
+      "бессрочных изгнаний.",
+  },
+  "en-US": {
+    title: "Rules",
+    text1:
+      "For most of the project's participants, it's more than just an online " +
+      "site—it's a home, a cozy and beloved place. To ensure everyone's stay " +
+      "here is not disrupted, a set of rules has been developed. By " +
+      "registering on the site, you essentially agree to abide by them.",
+    text2:
+      "In fact, the rules are very simple. If you simply play games peacefully, " +
+      "communicate in the chat and on the forum, and don't try to argue with " +
+      "anyone or get upset, you'll never have to face the consequences of breaking them!",
+    text3:
+      "We love all sorts of roleplaying slang here, so we call moderators " +
+      '"goblins" and administrators "trolls." Get used to it, because what\'s ' +
+      "next is about these evil goblinoids.",
+    text4:
+      "The site's public sections—forum threads, general chat, and user profiles " +
+      "are not the place for arguments, insults, swearing, and other internet " +
+      "delights. For these activities, the goblins award points according to a " +
+      "special price list. After snatching up these magical fruits, the user " +
+      "is briefly banished from these public sections of the site—but can still " +
+      "participate in games.",
+    text5:
+      "If a user persists in violating the rules of decency, they may be subject " +
+      "to full banishment. The length of bans, both full and what we call " +
+      '"democratic," increases with each instance, leading to permanent bans.',
+  },
+};
