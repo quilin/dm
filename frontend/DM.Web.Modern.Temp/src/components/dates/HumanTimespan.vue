b6ba2a0524ab4d29a287de57f57ab06bb0e640db
quilin <v.quilin@gmail.com>
2025-12-17 08:27:15 +0100

    [DM.Front] Fix date format locale

--- frontend/DM.Web.Modern.Temp/src/components/dates/HumanTimespan.vue
+++ frontend/DM.Web.Modern.Temp/src/components/dates/HumanTimespan.vue
@@ -5,8 +5,12 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import dayjs from "dayjs";
+import { useI18n } from "vue-i18n";
 
 const props = defineProps<{ date: string }>();
+const { locale } = useI18n();
 
-const humanTimespan = computed(() => dayjs(props.date).fromNow());
+const humanTimespan = computed(() =>
+  dayjs(props.date).locale(locale.value).fromNow(),
+);
 </script>
